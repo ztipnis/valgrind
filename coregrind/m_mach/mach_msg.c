46f4d0b817892b24262d9eaf8fa12557b7627f6d
Zachary Tipnis <tipniz@rpi.edu>
2019-08-09 11:25:16 -0400

    I don't even want to try random forum code...

--- coregrind/m_mach/mach_msg.c
+++ coregrind/m_mach/mach_msg.c
@@ -45,7 +45,7 @@
 #include <mach/mach.h>
 #include <mach/port.h>
 #include <mach/message.h>
-//#include <kern/ipc_mig.h>
+#include <kern/ipc_mig.h>
 
 #define LIBMACH_OPTIONS (MACH_SEND_INTERRUPT|MACH_RCV_INTERRUPT)
 
@@ -109,79 +109,6 @@ void
 mach_msg_destroy(mach_msg_header_t *msg)
 {
   // TODO: maybe copy from XNU?
-}
-
-void
-mach_msg_destroy_from_kernel_proper(
-    mach_msg_header_t    *msg)
-
-{
-  char v1;
-  mach_msg_type_number_t j;
-  ipc_object_t objects; 
-  mach_msg_bits_t i; 
-  mach_msg_descriptor_t *daddr; 
-  mach_port_t object; 
-  struct ipc_object *objecta; 
-  signed int mbits; 
-
-  mbits = msg->msgh_bits;
-  object = msg->msgh_remote_port;
-  if ( object && object != (mach_port_t)-1LL )
-    ipc_object_destroy(&object->ip_object, msg->msgh_bits & 0x1F);
-  v1 = 0;
-  if ( msg->msgh_voucher_port )
-    v1 = (int)msg->msgh_voucher_port != -1;
-  if ( !(~v1 & 1) )
-    return;
-  if ( mbits < 0 )
-  {
-    daddr = (mach_msg_descriptor_t *)&msg[1].msgh_size;
-    i = 0;
-    while ( i < msg[1].msgh_bits )
-    {
-      switch ( *((unsigned int*)&daddr->type + 2) >> 24 )
-      {
-        case 0:
-          if ( daddr->port.name )
-          {
-            if ( daddr->port.name != (mach_port_t)-1LL )
-              ipc_object_destroy(&daddr->port.name->ip_object, (*((unsigned int*)&daddr->type + 2) >> 16) & 0xFF);
-          }
-          break;
-        case 1:
-        case 3:
-          if ( *((unsigned int*)&daddr->type + 3) )
-          {
-            vm_map_copy_discard((vm_map_copy_t)daddr->port.name);
-          }
-          else if ( daddr->port.name )
-          {
-            break;
-          }
-          break;
-        case 2:
-          objects = &daddr->port.name->ip_object;
-          if ( *((unsigned int*)&daddr->type + 3) )
-          {
-            if ( !objects )
-              break;
-            for ( j = 0; j < *((unsigned int*)&daddr->type + 3); ++j )
-            {
-              objecta = (struct ipc_object*)*((unsigned int*)&objects->io_bits + j);
-              if ( objecta && objecta != (struct ipc_object*)-1LL )
-                ipc_object_destroy(objecta, (*((unsigned int*)&daddr->type + 2) >> 16) & 0xFF);
-            }
-            kfree(daddr->port.name, 8LL * *((unsigned int *)&daddr->type + 3));
-          }
-          break;
-        default:
-          break;
-      }
-      ++i;
-      ++daddr;
-    }
-  }
 }
 
 void mach_msg_destroy(mach_msg_header_t *msg)
